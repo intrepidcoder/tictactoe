e07a822a7ba35224768ce7bbdbc8512b4729e2b1
intrepidcoder <intrepidcoder@users.noreply.github.com>
2016-01-19 21:49:36 -0500

    Add server check for finished games to delete them
    After the game is finished, the players are unpaired so that they can
    play again (possibly with a different opponent). There is not client
    support for this yet.

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ io.on('connection', function(socket) {
 	addPlayer(socket);
 
 	socket.on('disconnect', function() {
-		if (socket.gameId) {
+		if (socket.paired) {
 			io.to(socket.gameId).emit('opponent-disconnected');
 			delete games[socket.gameId];
+			delete socket.gameId;
+			socket.paired = false;
 		}
 
 		if (socket === unpairedSocket) {
@@ -31,17 +33,27 @@ io.on('connection', function(socket) {
 		var game = games[socket.gameId];
 		index = parseInt(index, 10);
 
-		if (typeof index === 'number' && 0 <= index && index < 10 && game.players[game.turn].socket.id === socket.id && !game.filled[index]) {
+		if (socket.paired && typeof index === 'number' && 0 <= index && index < 10 && game.players[game.turn].socket.id === socket.id && typeof game.filled[index] !== 'number') {
+			game.filled[index] = game.turn;
 			game.turn++;
 			game.turn %= 2;
-			game.filled[index] = true;
 
 			io.to(game.players[game.turn].socket.id).emit('opponent-moved', index);
+
+			if (isFinished(game.filled)) {
+				game.players[0].socket.paired = false;
+				game.players[1].socket.paired = false;
+				delete games[socket.gameId];
+				delete game.players[0].socket.gameId;
+				delete game.players[1].socket.gameId;
+			}
 		}
 	});
 
 	socket.on('play-again', function() {
-		addPlayer(socket);
+		if (!socket.paired) {
+			addPlayer(socket);
+		}
 	});
 });
 
@@ -67,7 +79,24 @@ var addPlayer = function(socket) {
 		unpairedSocket = socket;
 		needsPair = true;
 	}
-}
+};
+
+var isFinished = function(filled) {
+	var positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+	var filledCount = 0;
+
+	for (var i = 0; i < positions.length; i++) {
+		if (typeof filled[positions[i][0]] === 'number' && filled[positions[i][0]] === filled[positions[i][1]] && filled[positions[i][1]] === filled[positions[i][2]]) {
+			return true;
+		}
+	}
+
+	for (var i = 0; i < filled.length; i++) {
+		if (typeof filled[i] === 'number') filledCount++;
+	}
+
+	return filledCount >= 9;
+};
 
 http.listen(3000, function() {
 	console.log('Listening on *:3000');
